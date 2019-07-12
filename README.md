45d07171e6afafb79ff785119e10da2c39afcb81
Parvind <mail2parvind@gmail.com>
2019-07-12 15:56:10 +0530

    added dependency injection

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ http://example.com/page_json/{site_api_key}/{node_id}
 Site API Key is configurable in the backend.
 http://example.com/admin/config/system/site-information
 
-
+Dependecies
+--------------
+The module uses the Drupal's core Serialization module to generate JSON.
--- src/Controller/PageJsonController.php
+++ src/Controller/PageJsonController.php
@@ -5,6 +5,8 @@ namespace Drupal\page_json\Controller;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\node\Entity\Node;
 
 /**
  * Controller for Page JSON.
@@ -14,12 +16,20 @@ class PageJsonController extends ControllerBase {
   /**
    * Gets JSON for a node.
    */
-  public function pageJson() {
-    $nid = \Drupal::routeMatch()->getParameter('nid');
-    $node_dt = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+  public function pageJson(RouteMatchInterface $route_match) {
+    $nid = $route_match->getParameter('nid');
+
+    if (!is_numeric($nid)) {
+      throw new AccessDeniedHttpException();
+    }
+    $node_dt = Node::load($nid);
+    if (empty($node_dt)) {
+      throw new AccessDeniedHttpException();
+    }
     $node_type = $node_dt->bundle();
+
     // API Key retrieved from Request URL.
-    $api_key = \Drupal::routeMatch()->getParameter('api_key');
+    $api_key = $route_match->getParameter('api_key');
     // API Key retrieved from Configuration.
     $config = \Drupal::service('config.factory')->getEditable('system.site');
     $siteapikey = $config->get('siteapikey');
